9fdcae99ebb6726e022e598c21632787fee74feb
Pedro <pborba361@gmail.com>
2024-06-29 12:09:40 -0300

    ajustes

--- aulas/pokedex/js/script.js
+++ aulas/pokedex/js/script.js
@@ -1,63 +1,68 @@
 /**
          * Função assíncrona para buscar e exibir informações de um Pokémon com base no ID fornecido.
          */
-async function fetchPokemon() {
-    // -- Obter o ID do pokemon pelo Usuario -->
+let pokemonId = document.getElementById("pokemon_id").value;
 
-    const pokemonId = document.getElementById("pokemon-id").value;
-   
-    // -- Ação para verificar o ID, se é ou não compativel -->
-    if (pokemonId) {
-        try {
-            // -- Procurando na API -->
-            const resposta = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`);
-            // -- Conversão para JSON -->
-            const pokemon = await resposta.json();
-
-            // -- Exibe as informações do Pokémon na página -->
-            const pokemonImagem = document.getElementById('pokemon-image');
-            pokemonImagem.src = pokemon.sprites.front_default;
-            alert("teste");
-            pokemonImagem.style.display = 'block';
-            document.getElementById('pokemon-nome').innerText = `Nome: ${pokemon.name}`;
-            document.getElementById('pokemon-tipos').innerText = `Tipos: ${pokemon.types.map(typeInfo => typeInfo.type.name).join(', ')}`;
-            document.getElementById('pokemon-peso').innerText = `Peso: ${pokemon.weight} kg`;
-            document.getElementById('pokemon-altura').innerText = `Altura: ${pokemon.height} m`;
-
-            // -- Reproduz o som do Pokémon, se não for encotrado será avisado. -->
-            if (pokemon.cries && pokemon.cries.latest) {
-                const pokemonSom = document.getElementById('pokemon-som');
-                pokemonSom.src = pokemon.cries.latest;
-                pokemonSom.play();
-            } else if (pokemon.cries && pokemon.cries.legacy) {
-                const pokemonSom = document.getElementById('pokemon-som');
-                pokemonSom.src = pokemon.cries.legacy;
-                pokemonSom.play();
-            } else {
-                console.log('Não encontrado.');
-            }
-
-        } catch (error) {
-            // -- Erros de informacoes ou JSON -->
-            console.error('Erro ao buscar informações do Pokémon:', error);
+async function buscarPokemon() {
+    try {
+        const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`);
+        if (APIResponse.status === 200) {
+            const data = await APIResponse.json();
+            exibirPokemon(data);
+        } else {
+            throw new Error("Pokemon not found");
         }
+    } catch (error) {
+        console.error(error);
+        return null;
+    }
+};
+
+
+// -- Obter o ID do pokemon pelo Usuario -->
+
+function exibirPokemon(pokemon) {
+    // -- Exibe as informações do Pokémon na página -->
+    const pokemonImagem = document.getElementById('pokemon_image');
+    pokemonImagem.src = ${pokemon.sprites.front_default};
+    
+    document.getElementById('pokemon_nome').innerText = `Nome: ${pokemon.name}`;
+    document.getElementById('pokemon_tipos').innerText = `Tipos: ${pokemon.types.map(typeInfo => typeInfo.type.name).join(', ')}`;
+    document.getElementById('pokemon_peso').innerText = `Peso: ${pokemon.weight} kg`;
+    document.getElementById('pokemon_altura').innerText = `Altura: ${pokemon.height} m`;
+
+    if (pokemon.cries && pokemon.cries.latest) {
+        const pokemonSom = document.getElementById('pokemon_som');
+        pokemonSom.src = pokemon.cries.latest;
+        pokemonSom.play();
+    } else if (pokemon.cries && pokemon.cries.legacy) {
+        const pokemonSom = document.getElementById('pokemon_som');
+        pokemonSom.src = pokemon.cries.legacy;
+        pokemonSom.play();
+    } else {
+        console.log('Não encontrado.');
     }
 }
+
+
+
+// -- Ação para verificar o ID, se é ou não compativel -->
+
+
+// -- Procurando na API -->
+
+
+
+// -- Exibe as informações do Pokémon na página -->
+
+
+// -- Reproduz o som do Pokémon, se não for encotrado será avisado. -->
+
+
+
+
+
 // -- Musica de fundo. -->
 let musicaFundo = true;
 
-function toggleMusic() {
-    const music = document.getElementById('background-music');
-    const musicIcon = document.getElementById('music-icon');
 
-    // -- Altera entre icones quando clicar para parar e retomar. -->
-    if (musicaFundo) {
-        music.pause();
-        musicaFundo = false;
-        musicIcon.src = 'img/sem-audio.png';
-    } else {
-        music.play();
-        musicaFundo = true;
-        musicIcon.src = 'img/com-audio.png';
-    }
-}
