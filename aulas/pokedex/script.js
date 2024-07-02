5524b47b4f384c2baa1c35d4ce585fe43e43fc15
Pedro <pborba361@gmail.com>
2024-07-02 19:24:41 -0300

    Atualizando

---
+++ aulas/pokedex/script.js
@@ -0,0 +1,91 @@
+/**
+         * Função assíncrona para buscar e exibir informações de um Pokémon com base no ID fornecido.
+         */
+let pokemonId = 1;
+const pokemonImagem = document.getElementById('pokemon_image');
+const pokemonInput = document.getElementById('pokemon_id');
+const btnPesquisar = document.getElementById('bsearch');
+const btnNext = document.getElementById('bnext');
+
+const fetchPokemon = async (pokemon) => {
+    const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+
+    if (APIResponse.status === 200) {
+        const data = await APIResponse.json();
+        return data;
+    }
+};
+
+
+// -- Obter o ID do pokemon pelo Usuario -->
+async function exibirPokemon(pokemon) {
+
+    const data = await fetchPokemon(pokemon)
+    if (data) {
+        pokemonImagem.style.display = "block";
+        pokemonImagem.src = data.sprites.front_default;
+        pokemonId = data.id;
+    }
+    else {
+        pokemonImagem.style.display = "none";
+    }
+    // -- Exibe as informações do Pokémon na página -->
+    document.getElementById('pokemon_nome').innerText = `Nome: ${pokemon.name}`;
+    document.getElementById('pokemon_tipos').innerText = `Tipos: ${pokemon.types.map(typeInfo => typeInfo.type.name).join(', ')}`;
+    document.getElementById('pokemon_peso').innerText = `Peso: ${pokemon.weight} kg`;
+    document.getElementById('pokemon_altura').innerText = `Altura: ${pokemon.height} m`;
+
+    if (pokemon.cries && pokemon.cries.latest) {
+        const pokemonSom = document.getElementById('pokemon_som');
+        pokemonSom.src = pokemon.cries.latest;
+        pokemonSom.play();
+    } else if (pokemon.cries && pokemon.cries.legacy) {
+        const pokemonSom = document.getElementById('pokemon_som');
+        pokemonSom.src = pokemon.cries.legacy;
+        pokemonSom.play();
+    } else {
+        console.log('Não encontrado.');
+    }
+}
+
+
+
+// -- Ação para verificar o ID, se é ou não compativel -->
+
+
+// -- Procurando na API -->
+
+
+
+// -- Exibe as informações do Pokémon na página -->
+
+
+// -- Reproduz o som do Pokémon, se não for encotrado será avisado. -->
+
+
+
+
+
+// -- Musica de fundo. -->
+let musicaFundo = true;
+
+
+btnPesquisar.addEventListener('click', (event) => {
+    event.preventDefault();
+    if (pokemonInput.value === "") {
+        return
+    }
+    exibirPokemon(pokemonInput.value.toLowerCase());
+});
+
+
+
+btnNext.addEventListener('click', (event) => {
+    event.preventDefault();
+    if (pokemonId < 1025) {
+        pokemonId += 1;
+        exibirPokemon(pokemonId);
+    }
+});
+
+exibirPokemon(pokemonId)
